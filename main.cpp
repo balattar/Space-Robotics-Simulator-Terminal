1b1cd359cfd8f1930d38e96cb20837c6f587f3b4
Bader AlAttar <baderalattar22@gmail.com>
2021-01-23 13:24:33 -0800

    main script to launch simulator

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Space Station Simulator
+//
+
+#include <iostream>
+#include "SpaceStation.h"
+#include "Astrobee.h"
+
+int main() {
+    
+    SpaceStation SS = SpaceStation();
+    
+    return 0;
+}
